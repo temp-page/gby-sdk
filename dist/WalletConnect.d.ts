b0ed28a18e80a19980dbcb5df6c6c73784aed0d3
lihang941 <1329075357@qq.com>
2024-03-12 11:00:23 +0000

    feat: 买卖

---
+++ dist/WalletConnect.d.ts
@@ -0,0 +1,44 @@
+import { type JsonRpcApiProvider, type Signer } from 'ethers6';
+import { ConnectInfo } from './ConnectInfo';
+export declare class WalletConnect {
+    wallet: any;
+    connectInfo: ConnectInfo;
+    provider?: any;
+    disconnectCallBack: () => void;
+    connectCallBack: () => void;
+    getChainId(): Promise<number>;
+    getAccount(): Promise<string>;
+    getWallet(): Promise<Signer>;
+    getApiProvider(): Promise<JsonRpcApiProvider>;
+    switchNetwork: (chainId: number) => void;
+    constructor(walletType: any, provider?: any);
+    disConnect(): void;
+    update(): void;
+    init(): Promise<void>;
+    static connectMetaMask(): Promise<WalletConnect>;
+    static getEthereum(): any;
+    /**
+     * 链接钱包
+     * @returns ConnectInfo
+     */
+    connect(): Promise<ConnectInfo>;
+}
+export declare class ConnectManager {
+    private static connectInfo;
+    private static walletConnect;
+    static chainMap: Record<string, any>;
+    /**
+     * 初始化
+     * @param wallet
+     */
+    static connect(wallet: WalletConnect): Promise<ConnectInfo>;
+    /**
+     * 断开连接
+     */
+    static disConnect(): Promise<void>;
+    /**
+     * 获取连接
+     */
+    static getConnect(): ConnectInfo;
+    static addMetamaskChain(chainName: string): void;
+}
