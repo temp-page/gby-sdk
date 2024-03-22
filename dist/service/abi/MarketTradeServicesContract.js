a3661b7fcb247413468c85964abf3d70a79ab422
lihang941 <1329075357@qq.com>
2024-03-22 02:38:13 +0000

    fix bugs

---
+++ dist/service/abi/MarketTradeServicesContract.js
@@ -0,0 +1,47 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MarketTradeServicesContract = void 0;
+const tool_1 = require("../tool");
+const BaseAbi_1 = require("./BaseAbi");
+const abi_1 = require("../../abi");
+let MarketTradeServicesContract = class MarketTradeServicesContract extends BaseAbi_1.BaseAbi {
+    constructor(connectInfo) {
+        super(connectInfo, connectInfo.chainInfo().GBU, abi_1.MarketTradeServicesAbi);
+    }
+    async openBoostTrade(isLong, targetID, tokenID, tokenAmount, leverage, takeProfit, nonce) {
+        return await this.connectInfo
+            .tx()
+            .sendContractTransaction(this.contract, 'openBoostTrade', [isLong, targetID, tokenID, tokenAmount, leverage, takeProfit, nonce], {});
+    }
+    async closeBoostTrade(positionHash, nonce) {
+        return await this.connectInfo
+            .tx()
+            .sendContractTransaction(this.contract, 'closeBoostTrade', [positionHash, nonce], {});
+    }
+};
+exports.MarketTradeServicesContract = MarketTradeServicesContract;
+__decorate([
+    (0, tool_1.EnableLogs)(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Boolean, String, String, String, String, String, String]),
+    __metadata("design:returntype", Promise)
+], MarketTradeServicesContract.prototype, "openBoostTrade", null);
+__decorate([
+    (0, tool_1.EnableLogs)(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, String]),
+    __metadata("design:returntype", Promise)
+], MarketTradeServicesContract.prototype, "closeBoostTrade", null);
+exports.MarketTradeServicesContract = MarketTradeServicesContract = __decorate([
+    (0, tool_1.CacheKey)('MarketTradeServicesContract'),
+    __metadata("design:paramtypes", [Function])
+], MarketTradeServicesContract);
---
+++ dist/service/api/TradeApi.d.ts
@@ -0,0 +1,35 @@
+import { ChainInfo, ChainType, ClosePositionEvent, OpenPosition, OpenPositionConfig, OpenPositionEvent, OpenPositionParams, PositionData, Target, TargetTick, TokenPriceBalance, TradeInfo } from "../vo";
+import { ConnectInfo } from "../../ConnectInfo";
+import { EventBus } from "../../wallet";
+export type EVNET_ID_INDEX = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+export declare class TradeEventBus extends EventBus {
+    static TOPIC_PRE_OPEN_POSITION: string;
+    static TOPIC_PAIR_PRICE: string;
+    static TOPIC_ALL_PRICE: string;
+    static TOPIC_POSITIONS: string;
+    static SYS_TOPIC_TICKS_UPDATE: string;
+    static SYS_TOPIC_POSITIONS: string;
+    emitAll(eventName: string, data?: any): void;
+    removeAll(eventName: string): void;
+}
+export declare class TradeApi {
+    pairs(chainType: ChainType): Promise<Target[]>;
+    targetTicks(): Promise<TargetTick[]>;
+    targetTickMap(): Promise<Record<string, TargetTick>>;
+    private startTickUpdateEvent;
+    private startPositionEvent;
+    private startEvent;
+    tradeInfo(chainType: ChainType, targetName: string): Promise<TradeInfo>;
+    positionHistories(chainType: ChainType, userAddress: string, page?: number, pageSize?: number): Promise<{
+        total: number;
+        list: PositionData[];
+    }>;
+    positions(tickPrices: Record<string, TargetTick>, chainType: ChainType, userAddress: string): Promise<PositionData[]>;
+    private updatePnl;
+    private positionRecordAbiResultToPositionData;
+    preOpenPosition(preOpenPositionParams: OpenPositionParams, tick: TargetTick, targetConfig: OpenPositionConfig): OpenPosition;
+    openPosition(connectInfo: ConnectInfo, preOpenPositionParams: OpenPositionParams, target: string): Promise<OpenPositionEvent>;
+    checkTradeState(txHash: string, chainInfo: ChainInfo): Promise<void>;
+    closePosition(positionHash: string, connectInfo: ConnectInfo): Promise<ClosePositionEvent>;
+    tokens(chainType: ChainType, account?: string | undefined): Promise<TokenPriceBalance[]>;
+}
