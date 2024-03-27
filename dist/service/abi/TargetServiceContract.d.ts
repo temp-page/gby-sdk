f08d468fb90a8a130682b788a2cf77ca59bfc833
lihang941 <1329075357@qq.com>
2024-03-27 02:20:38 +0000

    fix bugs

---
+++ dist/service/abi/TargetServiceContract.d.ts
@@ -0,0 +1,10 @@
+import { BaseAbi } from "./BaseAbi";
+import type { ConnectInfo } from "../../ConnectInfo";
+import { TransactionEvent } from "../vo";
+import { ContractCall } from "../../mulcall";
+export declare class TargetServiceContract extends BaseAbi {
+    constructor(connectInfo: ConnectInfo);
+    addTarget(name: string, targetID: string, targetType: number): Promise<TransactionEvent>;
+    removeTarget(targetID: string): Promise<TransactionEvent>;
+    multicall_listTarget(): ContractCall<[string[], string[]]>;
+}
---
+++ dist/wallet/index.d.ts
@@ -0,0 +1,5 @@
+export * from './BaseWallet';
+export * from './MetaMaskProvider';
+export * from './WalletConnectProvider';
+export * from './WalletProvider';
+export * from './UseVisibilityChange';
