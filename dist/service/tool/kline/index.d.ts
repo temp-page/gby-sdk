e1f00df428b7aaed8e6dfa93e8bfb0afc069d277
lihang941 <1329075357@qq.com>
2024-04-12 07:30:51 +0000

    fix bugs

---
+++ dist/service/tool/kline/index.d.ts
@@ -0,0 +1,10 @@
+export interface PriceLine {
+    time: number;
+    priceUSD: string;
+}
+export declare function generatePriceLine(timeInterval: number, endTime: number, size: number, contain: boolean, klineList: PriceLine[]): PriceLine[];
+/**
+ * K线聚合
+ * @param priceLines
+ */
+export declare function aggregateData(priceLines: PriceLine[]): PriceLine | undefined;
