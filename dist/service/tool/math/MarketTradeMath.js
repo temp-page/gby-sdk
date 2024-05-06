cb6fea2d55b1f807ccd17dc7ba152168b10b795f
lihang941 <1329075357@qq.com>
2024-05-06 08:00:52 +0000

    fix bugs

---
+++ dist/service/tool/math/MarketTradeMath.js
@@ -0,0 +1,238 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MarketTradeMath = void 0;
+const bignumber_js_1 = __importDefault(require("bignumber.js"));
+const vo_1 = require("../../vo/");
+class MarketTradeMath {
+    static margin(tokenAmount, tokenPrice, decimals) {
+        return new bignumber_js_1.default(tokenAmount)
+            .multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.QTY_DECIMALS))
+            .multipliedBy(tokenPrice)
+            .dividedBy(new bignumber_js_1.default(10).pow(decimals))
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    static notionalPositionValue(tokenAmount, leverage, tokenPrice, decimals) {
+        return new bignumber_js_1.default(tokenAmount)
+            .multipliedBy(leverage)
+            .dividedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .dividedBy(100)
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.QTY_DECIMALS))
+            .multipliedBy(tokenPrice)
+            .dividedBy(new bignumber_js_1.default(10).pow(decimals))
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    // uint256 openFee = (notionalPositionValue * targetConfig.openFeeRate) / 10 ** CONST.RATIO_DECIMALS;
+    static openFee(notionalPositionValue, openFeeRate) {
+        return new bignumber_js_1.default(notionalPositionValue)
+            .multipliedBy(openFeeRate)
+            .dividedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    //uint256 executeFeeByTokenAmount = ((targetConfig.execFeeValue) * (10 ** decimals)) /
+    //             tokenPrice /
+    //             (10 ** CONST.QTY_DECIMALS);
+    static executeFeeByTokenAmount(execFeeValue, decimals, tokenPrice) {
+        return new bignumber_js_1.default(execFeeValue)
+            .multipliedBy(new bignumber_js_1.default(10).pow(decimals))
+            .dividedBy(tokenPrice)
+            .dividedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.QTY_DECIMALS)).dp(0, bignumber_js_1.default.ROUND_DOWN).toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    //openTrade.isBoost
+    //             ? (notionalPositionValue * targetConfig.closeFeeRate) / 10 ** CONST.RATIO_DECIMALS
+    //             : 0;
+    static closeFee(isBoost, notionalPositionValue, closeFeeRate) {
+        if (isBoost) {
+            return new bignumber_js_1.default(notionalPositionValue)
+                .multipliedBy(closeFeeRate)
+                .dividedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))
+                .dp(0, bignumber_js_1.default.ROUND_DOWN)
+                .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+        }
+        else {
+            return "0";
+        }
+    }
+    //margin = margin - openFee - closeFee - targetConfig.execFeeValue;
+    static finalMargin(margin, openFee, closeFee, execFeeValue) {
+        return new bignumber_js_1.default(margin)
+            .minus(openFee)
+            .minus(closeFee)
+            .minus(execFeeValue)
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    static finalNetTradeValue(netTradeValue, execFeeValue, openFee, closeFee) {
+        return new bignumber_js_1.default(netTradeValue)
+            .minus(execFeeValue)
+            .minus(openFee)
+            .minus(closeFee)
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    //uint256 targetAmount = netTradeValue / targetPrice;
+    static targetAmount(netTradeValue, targetPrice) {
+        return new bignumber_js_1.default(netTradeValue)
+            .dividedBy(targetPrice)
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    static takeProfitPrice(isLong, takeProfit, margin, targetAmount, targetPrice) {
+        if (isLong) {
+            //openPositionArgs.takeProfitPrice =
+            //                     ((openTrade.takeProfit - 100 * 10 ** CONST.RATIO_DECIMALS) * margin) /
+            //                     (100 * 10 ** CONST.RATIO_DECIMALS) /
+            //                     targetAmount +
+            //                     targetPrice;
+            return new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS)).minus(takeProfit).multipliedBy(margin).dividedBy(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))).dividedBy(targetAmount).plus(targetPrice)
+                .dp(0, bignumber_js_1.default.ROUND_DOWN)
+                .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+        }
+        else {
+            //takeProfitPrice =
+            //                     targetPrice -
+            //                     (((openTrade.takeProfit - 100 * 10 ** CONST.RATIO_DECIMALS) * margin) /
+            //                         (100 * 10 ** CONST.RATIO_DECIMALS) /
+            //                         targetAmount);
+            return new bignumber_js_1.default(targetPrice)
+                .minus(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS)).minus(takeProfit).multipliedBy(margin).dividedBy(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))).dividedBy(targetAmount))
+                .dp(0, bignumber_js_1.default.ROUND_DOWN)
+                .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+        }
+    }
+    static liquidPrice(isLong, liquidation, margin, targetAmount, targetPrice) {
+        if (isLong) {
+            //liquidPrice = targetPrice -
+            //                     (((100 * 10 ** CONST.RATIO_DECIMALS - openTrade.liquidation) * margin) /
+            //                         (100 * 10 ** CONST.RATIO_DECIMALS) /
+            //                         targetAmount)
+            return new bignumber_js_1.default(targetPrice)
+                .minus(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS)).minus(liquidation).multipliedBy(margin).dividedBy(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))).dividedBy(targetAmount))
+                .dp(0, bignumber_js_1.default.ROUND_DOWN)
+                .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+        }
+        else {
+            // liquidPrice =
+            //   ((100 * 10 ** CONST.RATIO_DECIMALS - openTrade.liquidation) * margin) /
+            //   (100 * 10 ** CONST.RATIO_DECIMALS) /
+            //   targetAmount +
+            //   targetPrice;
+            return new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))
+                .minus(liquidation)
+                .multipliedBy(margin)
+                .dividedBy(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))).dividedBy(targetAmount).plus(targetPrice)
+                .dp(0, bignumber_js_1.default.ROUND_DOWN)
+                .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+        }
+    }
+    static stopLossPrice(isLong, positionType, stopLossPrice, stopLoss, margin, targetAmount, targetPrice) {
+        if (isLong) {
+            // if (openPositionArgs.positionType == CONST.POSITION_TYPE_BOOST) {
+            //                     openPositionArgs.stopLossPrice = uint256(0);
+            //                 } else {
+            //                     openPositionArgs.stopLossPrice =
+            //                         targetPrice -
+            //                         (((100 * 10 ** CONST.RATIO_DECIMALS - openTrade.stopLoss) * margin) /
+            //                             (100 * 10 ** CONST.RATIO_DECIMALS) /
+            //                             targetAmount);
+            //                 }
+            if (positionType == vo_1.CONST.POSITION_TYPE_BOOST) {
+                return new bignumber_js_1.default(0).toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+            }
+            else {
+                return new bignumber_js_1.default(targetPrice)
+                    .minus(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))
+                    .minus(stopLoss).multipliedBy(margin).dividedBy(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS)))
+                    .dp(0, bignumber_js_1.default.ROUND_DOWN)
+                    .dividedBy(targetAmount)
+                    .dp(0, bignumber_js_1.default.ROUND_DOWN)).toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+            }
+        }
+        else {
+            //   if (openPositionArgs.positionType == CONST.POSITION_TYPE_BOOST) {
+            //                     openPositionArgs.stopLossPrice = type(uint256).max;
+            //                 } else {
+            //                     openPositionArgs.stopLossPrice =
+            //                         ((100 * 10 ** CONST.RATIO_DECIMALS - openTrade.stopLoss) * margin) /
+            //                         (100 * 10 ** CONST.RATIO_DECIMALS) /
+            //                         targetAmount +
+            //                         targetPrice;
+            //                 }
+            if (positionType == vo_1.CONST.POSITION_TYPE_BOOST) {
+                return new bignumber_js_1.default(2).pow(256).minus(1).toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+            }
+            else {
+                return new bignumber_js_1.default(100)
+                    .multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS))
+                    .minus(stopLoss).multipliedBy(margin)
+                    .dividedBy(new bignumber_js_1.default(100).multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.RATIO_DECIMALS)))
+                    .dp(0, bignumber_js_1.default.ROUND_DOWN)
+                    .dividedBy(targetAmount)
+                    .dp(0, bignumber_js_1.default.ROUND_DOWN)
+                    .plus(targetPrice)
+                    .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+            }
+        }
+    }
+    //  (openPositionArgs.tokenAmount * tokenPrice * (10 ** CONST.QTY_DECIMALS)) / (10 ** decimals)
+    static entryValue(tokenAmount, tokenPrice, decimals) {
+        return new bignumber_js_1.default(tokenAmount)
+            .multipliedBy(tokenPrice)
+            .multipliedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.QTY_DECIMALS))
+            .dividedBy(new bignumber_js_1.default(10).pow(decimals))
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    static pnl(isLong, targetAmount, openPrice, targetPrice, marginAmount) {
+        const targetOpenValue = new bignumber_js_1.default(targetAmount).multipliedBy(openPrice);
+        const targetCloseValue = new bignumber_js_1.default(targetAmount).multipliedBy(targetPrice);
+        let pnl;
+        if (isLong) {
+            pnl = new bignumber_js_1.default(targetCloseValue).minus(targetOpenValue).toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+        }
+        else {
+            pnl = new bignumber_js_1.default(targetOpenValue).minus(targetCloseValue).toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+        }
+        /*if (new BigNumber(pnl).plus(marginAmount).comparedTo("0") > 0){
+          return pnl
+        }else {
+          return new BigNumber(marginAmount).negated().toFixed();
+        }*/
+        return pnl;
+    }
+    static tokenAmount(remainValue, tokenPrice, decimals) {
+        return new bignumber_js_1.default(remainValue)
+            .multipliedBy(new bignumber_js_1.default(10).pow(decimals))
+            .dividedBy(tokenPrice)
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .dividedBy(new bignumber_js_1.default(10).pow(vo_1.CONST.QTY_DECIMALS))
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    //uint256 totalFee = openFee + closeFee + targetConfig.execFeeValue;
+    static totalFee(openFee, closeFee, execFeeValue) {
+        return new bignumber_js_1.default(openFee)
+            .plus(closeFee)
+            .plus(execFeeValue)
+            .dp(0, bignumber_js_1.default.ROUND_DOWN)
+            .toFixed(0, bignumber_js_1.default.ROUND_DOWN);
+    }
+    static initTargetFundingFeeRate(targetTick, getTargetConfig) {
+        //fundingFeePerBlockP * 1e18 * 1 * 60 * 60  / 1e18 / 1e18
+        const fundingRate = new bignumber_js_1.default(getTargetConfig.fundingFeePerBlockP).multipliedBy(1e18).multipliedBy(1).multipliedBy(60).multipliedBy(60).div(1e18).div(1e18).toFixed();
+        //  fundingRateLong =fundingRate  *  （LongOI  - ShortOI） / LongIO
+        const fundingRateLong = new bignumber_js_1.default(targetTick.longOI).eq("0") ? "0" : new bignumber_js_1.default(fundingRate).multipliedBy(new bignumber_js_1.default(targetTick.shortOI).minus(new bignumber_js_1.default(targetTick.longOI))).div(new bignumber_js_1.default(targetTick.longOI)).multipliedBy(100).toFixed();
+        //  fundingRateShort =fundingRate  *  （ShortOI  - LongOI） / ShortOI
+        const fundingRateShort = new bignumber_js_1.default(targetTick.shortOI).eq("0") ? "0" : new bignumber_js_1.default(fundingRate).multipliedBy(new bignumber_js_1.default(targetTick.longOI).minus(new bignumber_js_1.default(targetTick.shortOI))).div(new bignumber_js_1.default(targetTick.shortOI)).multipliedBy(100).toFixed();
+        targetTick.fundingRateLong = fundingRateLong;
+        targetTick.fundingRateShort = fundingRateShort;
+    }
+}
+exports.MarketTradeMath = MarketTradeMath;
