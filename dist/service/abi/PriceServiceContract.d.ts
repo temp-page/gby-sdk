de094635b3196089c79bf1b60276d7ec96a18de8
lihang941 <1329075357@qq.com>
2024-07-10 02:10:28 +0000

    fix bugs

---
+++ dist/service/abi/PriceServiceContract.d.ts
@@ -0,0 +1,5 @@
+import { BaseAbi } from "./BaseAbi";
+import type { ConnectInfo } from "../../ConnectInfo";
+export declare class PriceServiceContract extends BaseAbi {
+    constructor(connectInfo: ConnectInfo);
+}
---
+++ dist/service/api/gql/PerpGraphGQL.d.ts
@@ -0,0 +1,28 @@
+import { GQLParams } from "./index";
+export interface PrepClaimShareLogsType {
+    claimShareLogs: {
+        timestamp: string;
+        hash: string;
+        tokenAddress: string;
+        amount: string;
+        token: {
+            tokenName: string;
+            tokenAddress: string;
+        };
+    }[];
+}
+export declare function PrepClaimShareLogsGql(account: string): GQLParams<PrepClaimShareLogsType>;
+export interface PrepIncreaseReferrerShareLogsType {
+    increaseReferrerShareLogs: {
+        timestamp: string;
+        volume: string;
+        fromUser: string;
+        amount: string;
+        token: {
+            tokenName: string;
+            tokenAddress: string;
+            decimals: any;
+        };
+    }[];
+}
+export declare function PrepIncreaseReferrerShareLogsGql(account: string): GQLParams<PrepIncreaseReferrerShareLogsType>;
