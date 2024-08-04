1f2f49c8da0a809bbd7ef26c1e98424a1337ad4b
lihang941 <1329075357@qq.com>
2024-08-04 12:45:08 +0000

    fix bugs

---
+++ dist/service/vo/referral/index.d.ts
@@ -0,0 +1,40 @@
+import { TransactionEvent } from "../TransactionEvent";
+import { ConnectInfo } from "../../../ConnectInfo";
+export interface ReferralInfo {
+    KOL: boolean;
+    kolData: ReferralKOLInfo;
+    bind: boolean;
+    bindData: BindData;
+    claim(connectInfo: ConnectInfo): Promise<TransactionEvent>;
+    bindCode(connectInfo: ConnectInfo, code: string): Promise<TransactionEvent>;
+}
+export interface ReferralKOLInfo {
+    code: string;
+    referrals: ReferralListData[];
+    claimAmounts: ReferralClaimAmounts[];
+    claimHistory: ReferralClaimHistory[];
+    canClaim: boolean;
+}
+export interface BindData {
+    disCount: string;
+}
+export interface ReferralClaimAmounts {
+    token: string;
+    amount: string;
+}
+export interface ReferralListData {
+    token: string;
+    address: string;
+    dateJoined: string;
+    totalVolume: string;
+    feesPaid: string;
+    totalOpenFee: string;
+    totalCloseFee: string;
+    totalFee: string;
+}
+export interface ReferralClaimHistory {
+    time: string;
+    txId: string;
+    claimToken: string;
+    claimAmount: string;
+}
