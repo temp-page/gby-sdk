be619c7388d3fcea5e89f8e3b2b1a4fbc186ca5e
lihang941 <1329075357@qq.com>
2024-08-19 13:30:17 +0000

    fix bugs

---
+++ dist/service/contract/index.d.ts
@@ -0,0 +1,30 @@
+/**
+ * ABI
+ */
+import type { JsonFragment } from 'ethers6';
+export declare const IERC20Abi: JsonFragment[];
+export * from './IERC20Contract';
+export declare const Multicall2Abi: JsonFragment[];
+export * from './Multicall2Contract';
+export declare const USDTAbi: JsonFragment[];
+export * from './USDTContract';
+export declare const VaultServiceAbi: JsonFragment[];
+export * from './VaultServiceContract';
+export declare const LpServiceAbi: JsonFragment[];
+export * from './LpServiceContract';
+export declare const MarketTradeServicesAbi: JsonFragment[];
+export * from './MarketTradeServicesContract';
+export declare const BoostTradeServicesAbi: JsonFragment[];
+export * from './BoostTradeServicesContract';
+export declare const TargetServiceAbi: JsonFragment[];
+export * from './TargetServiceContract';
+export declare const PositionServiceAbi: JsonFragment[];
+export * from './PositionServiceContract';
+export declare const PriceServiceAbi: JsonFragment[];
+export * from './PriceServiceContract';
+export declare const RoleServiceAbi: JsonFragment[];
+export * from './RoleServiceContract';
+export declare const ReferralServiceAbi: JsonFragment[];
+export * from './ReferralServiceContract';
+export declare const IpythAbi: JsonFragment[];
+export * from './IpythContract';
