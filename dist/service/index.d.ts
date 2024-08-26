e7ad3363dfa99802c4e649402a0e4cfe645e8854
lihang941 <1329075357@qq.com>
2024-08-26 06:50:05 +0000

    fix bugs

---
+++ dist/service/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * 业务接口实现
+ */
+export * from './vo/index';
+export * from './contract/index';
+export * from './tool/index';
+export * from './api/index';
+export * from './BaseService';
+export * from './Erc20Service';
+export * from './TransactionService';
+export * from './MultiCallService';
