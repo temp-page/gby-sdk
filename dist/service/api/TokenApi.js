39e3789a4035e0959c8ae603638f5ace172232a2
lihang941 <1329075357@qq.com>
2025-04-30 09:05:48 +0000

    fix bugs

---
+++ dist/service/api/TokenApi.js
@@ -0,0 +1,75 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TokenApi = void 0;
+const tool_1 = require("../tool");
+const vo_1 = require("../vo");
+const BaseApi_1 = require("./base/BaseApi");
+const contract_1 = require("../contract");
+let TokenApi = class TokenApi {
+    async vaultTokenList(chainType) {
+        const addressInfo = BaseApi_1.BASE_API.address();
+        const chainInfo = addressInfo.getChainInfo(chainType);
+        const connectInfo = addressInfo.readonlyConnectInfo(chainInfo);
+        const vaultServiceContract = connectInfo.create(contract_1.VaultServiceContract);
+        const [{ listTokens }] = await connectInfo.multiCall().callObj({
+            listTokens: vaultServiceContract.multicall.listTokens()
+        });
+        const [tokens,] = await Promise.all([
+            this.batchGetTokens(chainInfo, listTokens.tokenAddress)
+        ]);
+        const vaultTokens = listTokens.tokenAddress.map((it, index) => {
+            const vaultToken = {
+                address: it,
+                assetID: listTokens.assetID[index],
+                token: tokens[it],
+                weight: listTokens.targetWeights[index],
+                tokenAmount: listTokens.tokenAmounts[index],
+                priceId: listTokens.priceID[index]
+            };
+            return vaultToken;
+        });
+        return vaultTokens;
+    }
+    async batchGetTokens(chainInfo, addresses) {
+        const tokens = await this.getTokenByContract(chainInfo, addresses);
+        const tokenMap = {};
+        addresses.forEach((it, index) => {
+            tokenMap[it] = tokens[index];
+        });
+        return tokenMap;
+    }
+    async getTokenByContract(chainInfo, addresses) {
+        try {
+            const addressInfo = BaseApi_1.BASE_API.address();
+            const tokenInfos = await addressInfo.readonlyConnectInfo(chainInfo).erc20().batchGetTokenInfo(...addresses);
+            return tokenInfos
+                .filter(it => !(0, tool_1.isNullOrBlank)(it.name) && !(0, tool_1.isNullOrBlank)(it.symbol) && it.decimals > 0)
+                .map((it) => {
+                return new tool_1.Token(chainInfo.chainId, it.address, it.decimals, it.symbol, it.name, `${addressInfo.baseApiUrl}/static/${it.symbol.toLowerCase()}.png`);
+            });
+        }
+        catch (e) {
+            tool_1.Trace.error('getTokenByContract error ignore', e);
+            return [];
+        }
+    }
+};
+exports.TokenApi = TokenApi;
+__decorate([
+    (0, tool_1.MethodCache)("TokenApi.vaultTokenList.${args[0]}", 3 * 1000),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Promise)
+], TokenApi.prototype, "vaultTokenList", null);
+exports.TokenApi = TokenApi = __decorate([
+    (0, tool_1.CacheKey)('TokenApi')
+], TokenApi);
