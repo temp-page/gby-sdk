0c92f1c27cfd2e284534e15b77acf21af5cab760
lihang941 <1329075357@qq.com>
2025-06-25 07:43:30 +0000

    fix bugs

---
+++ dist/service/api/base/BaseApi.d.ts
@@ -0,0 +1,13 @@
+import type { Variables } from 'graphql-request';
+import type { ConnectInfo } from '../../../ConnectInfo';
+import { AddressInfo, ChainInfo, ChainType } from '../../vo';
+import { GQLParams } from "../gql";
+export declare class BaseApi {
+    request<T = any>(path: string, method: 'get' | 'post' | 'put' | 'delete', data: any, config?: any): Promise<T>;
+    graphBase<T = any, V = Variables>(fullUrl: string, query: string, variables: V): Promise<T>;
+    prepGraph<T>(params: GQLParams<T>): Promise<T>;
+    connectInfo(chainInfo: ChainInfo): ConnectInfo;
+    getCurrentConnectInfo(chainType: ChainType): ConnectInfo;
+    address(): AddressInfo;
+}
+export declare const BASE_API: BaseApi;
